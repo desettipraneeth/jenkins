1d3c6ebe14e4ecc2d97072fd325856f0dc76c6d5
Praneeth-at-990247146006 <praneeth@cricclubs.com>
2024-08-08 15:48:39 +0530

    foor loop

--- jenkinsproject/src/com/jenkinsproject/one/Demo.java
+++ jenkinsproject/src/com/jenkinsproject/one/Demo.java
@@ -6,7 +6,10 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        System.out.println("hello from java");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
 
     }
 }
